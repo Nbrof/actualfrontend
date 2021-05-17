f6537e46b7049d5c546173e8bd482c0b764d9a0f
Nikolaus Brodfuehrer <nbrof@Nikolauss-MacBook-Air.local>
2021-05-17 16:24:54 -0400

    Frontend

---
+++ src/Display.js
@@ -0,0 +1,31 @@
+import React from "react";
+const Display = (props) => {
+  // destruct the dogs from props
+  const {dogs, selectDog, history} = props
+  // Returns the JSX for when you have dogs
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+        <article key={dog._id}>
+          <img src={dog.img}/>
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+          <button onClick={() => {
+            selectDog(dog)
+            history.push("/edit")
+          }}>
+            edit
+          </button>
+          <button onClick={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>
+        </article>
+      ))}
+    </div>
+  )
+  const loading = () => <h1>Loading</h1>
+  return dogs.length > 0 ? loaded() : loading()
+};
+export default Display;
